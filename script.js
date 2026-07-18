af2fe2d7c43b3e45cf84082199141aca9225a73b
Wade Helquist <whelqui@wgu.edu>
2026-07-18 08:44:06 -0600

    Scale game board and refresh color handling

--- script.js
+++ script.js
@@ -1,180 +1,174 @@
-/*------------------------------CREATED BY WADE HELQUIST--------------------------
---------------------------------LAST UPDATE 8/29/2017------------------------------*/
-                    
-/*-----------GOT THIS CODE TO CONSOLIDATE PROGRAM ONLINE---------*/                    
-/* https://stackoverflow.com/questions/45909017/consolidating-html-code-button */
-var gridSize = prompt("Pick a grid-size 1-10","3"), buttons=[], array_of_ids=[];
-for (var i=1;i<=gridSize;i++) {
-  for (var j=1;j<=gridSize;j++) {
-    id = 's'+i+''+j;
-    buttons.push('<button type="button" onclick="sc('+i+', '+j+')" id="'+id+'" class="but blue">X</button>');
+/* Created by Wade Helquist */
+var BLUE = "#38bdf8";
+var RED = "#fb7185";
+
+var requestedSize = Number(prompt("Pick a grid size from 1 to 10", "3"));
+var gridSize = Number.isInteger(requestedSize) && requestedSize >= 1 && requestedSize <= 10
+  ? requestedSize
+  : 3;
+
+var tileSize = Math.max(24, Math.min(52, Math.floor((280 - gridSize * 4) / gridSize)));
+document.documentElement.style.setProperty("--tile-size", tileSize + "px");
+
+var buttons = [];
+var array_of_ids = [];
+
+for (var i = 1; i <= gridSize; i++) {
+  for (var j = 1; j <= gridSize; j++) {
+    var id = "s" + i + j;
+    buttons.push('<button type="button" aria-label="Tile ' + i + ', ' + j +
+      '" onclick="sc(' + i + ", " + j + ')" id="' + id + '" class="but">X</button>');
     array_of_ids.push(id);
-    if (j==gridSize) buttons.push('<br/>');
-  }  
+    if (j === gridSize) buttons.push("<br>");
+  }
 }
-document.querySelector("#container").innerHTML=buttons.join("")
-/*----------------------------------------------------------------*/
-var count = 0, nextSolution = 'O', totalTime = 0, bestTime = 0;
+
+document.querySelector("#container").innerHTML = buttons.join("");
+
+var count = 0;
+var nextSolution = "O";
+var totalTime = 0;
+var bestTime = 0;
 var time_string = "";
-document.getElementById("nextSolution").innerHTML = "<font size='+2'>Solve for<font size='+3' color='blue'><b> BLUE</b></font>.";
+
+setGoal("BLUE", BLUE);
 changeColor();
-randomize (gridSize*gridSize);
-// Example of 5x5 squares for functions:
-/*               1        2           3           4         5  
-            _____________________________________________________
-           |          |          |           |         |         |
-      1    |    11    |   12     |    13     |   14    |    15   |
-           |__________|__________|___________|_________|_________|
-           |          |          |           |         |         | 
-      2    |    21    |   22     |    23     |   24    |    25   | 
-           |__________|__________|___________|_________|_________| 
-           |          |          |           |         |         |
-      3    |    31    |   32     |    33     |   34    |    35   |
-           |__________|__________|___________|_________|_________|    
-           |          |          |           |         |         |
-      4    |    41    |   42     |    43     |   44    |    45   |
-           |__________|__________|___________|_________|_________| 
-           |          |          |           |         |         |
-      5    |    51    |   52     |    53     |   54    |    55   |
-           |__________|__________|___________|_________|_________|    
-           */
-/*----------------------TIMER-----------------------------------*/
-/* Found this online: https://stackoverflow.com/questions/20035357/display-current-time-from-javascript-count-up-timer/20035778#20035778 */
-        var minutesLabel = document.getElementById("minutes");
-        var secondsLabel = document.getElementById("seconds");
-        
-        var totalSeconds = 0; 
-        setInterval(setTime, 1000);
-        function setTime(){
-            ++totalSeconds;   
-            secondsLabel.innerHTML = pad(totalSeconds%60);
-            minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
-        }
-        function pad(val){
-            var valString = val + "";
-            if(valString.length < 2)
-              return "0" + valString;
-            else
-              return valString; 
-        } 
-        
-function pause(){
+randomize(gridSize * gridSize);
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60, 10));
+}
+
+function pad(val) {
+  return val < 10 ? "0" + val : String(val);
+}
+
+function pause() {
   alert("Click OK when ready to continue");
 }
-/*-------------------RANDOMIZE-------------------------*/
-function randomize(i){  
-       for (var loop =0; loop <= i; loop++){
-       sc(Math.floor(Math.random() * gridSize) + 1,
-       Math.floor(Math.random() * gridSize) + 1);     
-       solve();
-       }
+
+function randomize(turns) {
+  for (var loop = 0; loop <= turns; loop++) {
+    sc(
+      Math.floor(Math.random() * gridSize) + 1,
+      Math.floor(Math.random() * gridSize) + 1
+    );
+    solve();
+  }
 }
-/*-------------------INDIVIDUAL SQUARES--------------*/
-       
-function s(x){
- if (document.getElementById(x).innerText == 'O')
-      document.getElementById(x).innerText = 'X';
-  else 
-     document.getElementById(x).innerText = 'O';
+
+function s(tileId) {
+  var tile = document.getElementById(tileId);
+  tile.innerText = tile.innerText === "O" ? "X" : "O";
 }
-/*---------------------ON CLICK----------------------*/
-function sc(row, col){
- 
-  s("s" + row + col);  //Square that is clicked
-  if((row-1) > 0) // One Above
-    s("s" + (row-1) + col); 
-  
-  if((row + 1) <= gridSize) //One below
-    s("s" + (row+1) +col); 
-  
-  if((col - 1) > 0) //One left
-    s("s" + row + (col-1));
-  
-  if((col + 1) <= gridSize) //One right
-    s("s" + row +(col+1));
+
+function sc(row, col) {
+  s("s" + row + col);
+
+  if (row - 1 > 0) s("s" + (row - 1) + col);
+  if (row + 1 <= gridSize) s("s" + (row + 1) + col);
+  if (col - 1 > 0) s("s" + row + (col - 1));
+  if (col + 1 <= gridSize) s("s" + row + (col + 1));
+
   solve();
 }
-/*------------------------SOLVE---------------------------*/
-function solve(){
-      
-       /*------------IF RED NEXT SOLUTION IS BLUE--------*/
-if (nextSolution == 'X' && totalSeconds > 0 && changeColor()==(gridSize*gridSize)){   
-      nextSolution = 'O';
-      document.getElementById("nextSolution").innerHTML = "<font size='+2'>Solve for<font size='+3' font color='blue'><b> BLUE</font></b>";
-      color1(["time","minutes","seconds","title"],"blue",["count","stats","stats2"],"red");
-      color2("button1","blue"); color2("button2","red");
 
-      //https://stackoverflow.com/questions/15071062/using-javascript-to-edit-css-gradient/15071347
-      var dom = document.getElementById('background'); dom.style.backgroundImage = " radial-gradient(blue,red)";
-      solveEnd(); 
-      }      
-       /*------------IF BLUE NEXT SOLUTION IS RED--------*/
-if (nextSolution == 'O' && totalSeconds > 0 && changeColor()==(gridSize*gridSize)){   
-      nextSolution = 'X';
-      document.getElementById("nextSolution").innerHTML = "<font size='+2'>Solve for<font size='+3' font color='red'><b> RED</font></b>";
-      
-      //document.getElementById("time").style.color = "red";
-      color1(["time","minutes","seconds","title"],"red",["count","stats","stats2"],"blue");
-      color2("button1","red"); color2("button2","blue");
+function solve() {
+  if (nextSolution === "X" && totalSeconds > 0 && changeColor() === gridSize * gridSize) {
+    nextSolution = "O";
+    setGoal("BLUE", BLUE);
+    setTheme(BLUE, RED, false);
+    solveEnd();
+  }
 
-      // https://stackoverflow.com/questions/15071062/using-javascript-to-edit-css-gradient/15071347
-      var dom = document.getElementById('background'); dom.style.backgroundImage = " radial-gradient(red,blue)";
-      solveEnd();
-      }   
-      changeColor();   
+  if (nextSolution === "O" && totalSeconds > 0 && changeColor() === gridSize * gridSize) {
+    nextSolution = "X";
+    setGoal("RED", RED);
+    setTheme(RED, BLUE, true);
+    solveEnd();
+  }
+
+  changeColor();
 }
-          /*----PROVIDES STATS AND STARTS BOARD OVER---*/
-          
-function solveEnd(){
-  if (bestTime == 0)
-    bestTime = totalSeconds;
-    
-  if (bestTime > 0 && totalSeconds < bestTime)
-     bestTime = totalSeconds
-     
+
+function setGoal(name, color) {
+  document.getElementById("nextSolution").innerHTML =
+    'Solve for <strong style="color:' + color + '">' + name + "</strong>.";
+}
+
+function setTheme(primary, secondary, redTurn) {
+  color1(["minutes", "seconds"], primary, ["count", "stats", "stats2"], secondary);
+  color2("button1", primary);
+  color2("button2", secondary);
+
+  var background = document.getElementById("background");
+  background.style.backgroundImage = redTurn
+    ? "linear-gradient(145deg, rgba(48, 18, 35, .98), rgba(8, 12, 27, .98))"
+    : "linear-gradient(145deg, rgba(19, 27, 52, .98), rgba(8, 12, 27, .98))";
+}
+
+function solveEnd() {
+  if (bestTime === 0 || totalSeconds < bestTime) bestTime = totalSeconds;
+
   totalTime += totalSeconds;
   count += 1;
-  document.getElementById("count").innerHTML = "You have solved this <b>" + count +"</b> times in: " + min_and_sec(totalTime)
-  document.getElementById("stats").innerHTML = "Thats an average of: " + min_and_sec((totalTime / count))
-  document.getElementById("stats2").innerHTML = "Your best time is " + min_and_sec(bestTime)
-  alert("You solved it!");  
+
+  document.getElementById("count").innerHTML =
+    "Solved <b>" + count + "</b> time" + (count === 1 ? "" : "s") + " in " + min_and_sec(totalTime);
+  document.getElementById("stats").innerHTML =
+    "Average: " + min_and_sec(totalTime / count);
+  document.getElementById("stats2").innerHTML =
+    "Best: " + min_and_sec(bestTime);
+
+  alert("You solved it!");
   totalSeconds = 0;
-  randomize(gridSize*gridSize);   
-}
-    /*----------GIVES BOARD COLOR AND COUNTS FOR WIN--------------*/
-function changeColor(){
-var win_count = 0;
-for (i = 0; i < (gridSize*gridSize); i++){
-  if (document.getElementById(array_of_ids[i]).innerText == 'O'){
-      document.getElementById(array_of_ids[i]).style.background='blue';
-      document.getElementById(array_of_ids[i]).style.color = 'blue'; 
-      if (nextSolution == 'O') win_count += 1
-      }
-  else {document.getElementById(array_of_ids[i]).style.background='red';
-       document.getElementById(array_of_ids[i]).style.color = 'red'; 
-       if (nextSolution == 'X') win_count += 1
-      } 
-  }
-  return win_count
-}
- 
-function min_and_sec(time){
-  if (time > 60)
-    return time_string = "<b>" + (time / 60).toFixed(0) + 
-    "</b> minutes <b>" + (time % 60).toFixed(0) + "</b> seconds."
-  else 
-    return "<b>" + time.toFixed(0) + "</b>" + " seconds."
+  randomize(gridSize * gridSize);
 }
 
-function color1(id, color, id2, color2){
-    for(var i=0; i< id.length; i++) {
-      document.getElementById(id[i]).style.color = color; 
-    }
-    for(var i=0; i< id2.length; i++) {
-      document.getElementById(id2[i]).style.color = color2;  
+function changeColor() {
+  var win_count = 0;
+
+  for (var i = 0; i < gridSize * gridSize; i++) {
+    var tile = document.getElementById(array_of_ids[i]);
+
+    if (tile.innerText === "O") {
+      tile.style.background = BLUE;
+      if (nextSolution === "O") win_count += 1;
+    } else {
+      tile.style.background = RED;
+      if (nextSolution === "X") win_count += 1;
     }
   }
 
-function color2(id, color){
-   document.getElementById(id).style.background=color;
-    }
+  return win_count;
+}
+
+function min_and_sec(time) {
+  if (time >= 60) {
+    return "<b>" + Math.floor(time / 60) + "</b>m <b>" + Math.floor(time % 60) + "</b>s";
+  }
+
+  return "<b>" + time.toFixed(0) + "</b>s";
+}
+
+function color1(ids, color, secondaryIds, secondaryColor) {
+  for (var i = 0; i < ids.length; i++) {
+    document.getElementById(ids[i]).style.color = color;
+  }
+
+  for (var j = 0; j < secondaryIds.length; j++) {
+    document.getElementById(secondaryIds[j]).style.color = secondaryColor;
+  }
+}
+
+function color2(id, color) {
+  document.getElementById(id).style.background = color;
+}
